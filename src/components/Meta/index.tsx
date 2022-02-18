4c36aa2126e06c6528fca60f09d13e4a2c34a08f
Arinze Anozie <ariano@Arinzes-MacBook-Pro.local>
2022-02-18 13:20:03 -0500

    feat(seo): add meta component

---
+++ src/components/Meta/index.tsx
@@ -0,0 +1,37 @@
+import { NextSeo } from "next-seo";
+import Head from "next/head";
+
+type IMetaProps = {
+  title: string;
+  description: string;
+  canonical?: string;
+};
+
+const Meta = (props: IMetaProps) => {
+  return (
+    <>
+      <Head>
+        <meta charSet="UTF-8" key="charset" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1"
+          key="viewport"
+        />
+      </Head>
+      <NextSeo
+        title={props.title}
+        description={props.description}
+        canonical={props.canonical}
+        openGraph={{
+          title: props.title,
+          description: props.description,
+          url: props.canonical,
+          locale: "en",
+          site_name: "Ariano.ca",
+        }}
+      />
+    </>
+  );
+};
+
+export default Meta;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Meta from "@/components/Meta";
 import TopNav from "@/components/TopNav";
 import { getSortedPostsData, IPostData, PostsDirectory } from "@/lib/posts";
 import "@/styles/index.sass";
@@ -11,55 +12,61 @@ interface IHomeProps {
 
 const Home: NextPage<IHomeProps> = (props) => {
   return (
-    <div id="index">
-      <div id="container">
-        <TopNav className="hide-small" flat />
-        <div id="flexcontainer-index" className="flexcontainer">
-          <div id="text">
-            <h1 className="block">
-              <Link href="/about">
-                <a>About</a>
-              </Link>
-            </h1>
-            <p>
-              I'm Arinze, a cloud solutions architect currently consulting with
-              Bond Brand Loyalty and previously Bank of Montreal.
-            </p>
-            <p>
-              I most often talk about working with .NET in the cloud, solution &
-              application architecture, bleeding edge tech, and other
-              interesting tidbits.
-            </p>
+    <>
+      <Meta
+        title="Home - ariano.ca"
+        description="The personal website of Arinze Anozie and tech blog coming to you from Toronto, CA."
+      />
+      <div id="index">
+        <div id="container">
+          <TopNav className="hide-small" flat />
+          <div id="flexcontainer-index" className="flexcontainer">
+            <div id="text">
+              <h1 className="block">
+                <Link href="/about">
+                  <a>About</a>
+                </Link>
+              </h1>
+              <p>
+                I'm Arinze, a cloud solutions architect currently consulting
+                with Bond Brand Loyalty and previously Bank of Montreal.
+              </p>
+              <p>
+                I most often talk about working with .NET in the cloud, solution
+                & application architecture, bleeding edge tech, and other
+                interesting tidbits.
+              </p>
 
-            <div id="writing">
-              <div className="divider-short"></div>
-              <PostsList directory="articles" posts={props.articles} />
-              <div className="divider-short"></div>
-              <PostsList directory="fragments" posts={props.fragments} />
+              <div id="writing">
+                <div className="divider-short"></div>
+                <PostsList directory="articles" posts={props.articles} />
+                <div className="divider-short"></div>
+                <PostsList directory="fragments" posts={props.fragments} />
+              </div>
             </div>
-          </div>
 
-          <div id="photo">
-            <img
-              src="https://images.unsplash.com/photo-1446776811953-b23d57bd21aa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&q=75&w=1500"
-              alt="Ocean clouds seen from space"
-            />
-            <p>
-              Photo by{" "}
-              <Link href="https://unsplash.com/photos/yZygONrUBe8">
-                <a>NASA</a>
-              </Link>{" "}
-              on{" "}
-              <Link href="https://unsplash.com">
-                <a>Unsplash</a>
-              </Link>
-            </p>
-          </div>
+            <div id="photo">
+              <img
+                src="https://images.unsplash.com/photo-1446776811953-b23d57bd21aa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&q=75&w=1500"
+                alt="Ocean clouds seen from space"
+              />
+              <p>
+                Photo by{" "}
+                <Link href="https://unsplash.com/photos/yZygONrUBe8">
+                  <a>NASA</a>
+                </Link>{" "}
+                on{" "}
+                <Link href="https://unsplash.com">
+                  <a>Unsplash</a>
+                </Link>
+              </p>
+            </div>
 
-          <TopNav className="hide-big" flat />
+            <TopNav className="hide-big" flat />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
