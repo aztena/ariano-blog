9cd864b4f57522f01d79b9f1fe94b666aabe38c8
Arinze Anozie <ariano@Arinzes-MacBook-Pro.local>
2022-02-17 22:42:02 -0500

    feat: add sass and base styles

---
+++ src/pages/_document.tsx
@@ -0,0 +1,23 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+// Need to create a custom _document because i18n support is not compatible with `next export`.
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond&family=IBM+Plex+Mono&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
