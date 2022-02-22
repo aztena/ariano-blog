923b860b709bd8ae755f396187ba0764c72df97f
aztena <arinze.anozie@aetainvestments.com>
2022-02-21 23:33:44 -0500

    feat(markdown): add rehype plugin for figure markdown syntax

--- src/lib/markdown.ts
+++ src/lib/markdown.ts
@@ -1,17 +1,17 @@
 import { unified } from "unified";
 import remarkParse from "remark-parse";
 import remarkRehype from "remark-rehype";
-import rehypeDocument from "rehype-document";
 import rehypeFormat from "rehype-format";
 import rehypeStringify from "rehype-stringify";
 import rehypePrism from "rehype-prism-plus";
+import { figureTransformer } from "./rehype-plugins/figure";
 
 export async function generateHtmlFromMarkdown(content: string) {
   const vFile = await unified()
     .use(remarkParse)
     .use(remarkRehype)
-    .use(rehypeDocument)
     .use(rehypeFormat)
+    .use(() => figureTransformer)
     .use(rehypeStringify)
     .use(rehypePrism)
     .process(content);
---
+++ src/lib/rehype-plugins/figure.ts
@@ -0,0 +1,76 @@
+import { visit } from "unist-util-visit";
+import { h } from "hastscript";
+import type { Root } from "hast";
+
+interface HtmlImage {
+  src: string;
+  loading?: string;
+  alt: string;
+  className?: string;
+}
+
+export function figureTransformer(tree: Root) {
+  visit(tree, { tagName: "p" }, (node, index) => {
+    if (
+      node.children.length !== 1 ||
+      node.children[0]?.type !== "text" ||
+      index === null
+    )
+      return;
+
+    const textNode = node.children[0];
+    const matches = textNode.value.match(
+      /{{(Figure|FigureSingle|FigureSingleWithClass) ("[^"]+"\s?)+}}/gi
+    );
+
+    if (!matches) return;
+
+    const figureType = textNode.value
+      .match(/{{(Figure|FigureSingle|FigureSingleWithClass)\s/)
+      ?.at(1)!;
+
+    const parameters = (textNode.value.match(/("[^"]+)"\s?/gi) as string[]).map(
+      (value) => value.trim().replaceAll('"', "")
+    );
+
+    const caption = parameters[0]!;
+    switch (figureType) {
+      case "FigureSingle":
+        tree.children[index] = buildFigure(caption, {
+          src: parameters[1] || "",
+          alt: caption,
+        });
+        break;
+      case "FigureSingleWithClass":
+        tree.children[index] = buildFigure(caption, {
+          src: parameters[1] || "",
+          className: parameters[2] || "",
+          alt: caption,
+        });
+        break;
+      default:
+        buildFigure(
+          caption,
+          ...parameters.slice(1).map((src) => ({
+            src,
+            className: "",
+            alt: caption,
+          }))
+        );
+    }
+  });
+}
+
+function buildFigure(caption: string, ...imgs: HtmlImage[]) {
+  return h("figure", [
+    ...imgs.map((img) =>
+      h("img", {
+        src: img.src,
+        alt: caption,
+        loading: "lazy",
+        class: img.className || "",
+      })
+    ),
+    h("figcaption", caption),
+  ]);
+}
